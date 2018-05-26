92c5429975114df52ce4fb6a5a4fa5566b15d459
Alex Addy <alex.addy@gmail.com>
2018-05-26 07:50:57 -0400

    Got some starter code to show a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+extern crate amethyst;
+
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat, Event, KeyboardInput, Pipeline, PosNormTex,
+                         RenderBundle, Stage, VirtualKeyCode, WindowEvent};
+
+struct Snake;
+
+impl<'a, 'b> State<GameData<'a, 'b>> for Snake {
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => Trans::Quit,
+                _ => Trans::None,
+            },
+            _ => Trans::None,
+        }
+    }
+
+    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        data.data.update(&data.world);
+        Trans::None
+    }
+}
+
+fn run() -> Result<(), amethyst::Error> {
+    let display_path = "./resources/display_config.ron";
+    let config = DisplayConfig::load(&display_path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosNormTex>::new()),
+    );
+
+    let game_data = GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::<GameData>::new("./", Snake, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        println!("Failed to execute example: {}", e);
+        ::std::process::exit(1);
+    }
 }
